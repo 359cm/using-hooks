051d7db7be5323b6213a92483efbe028cf81ae78
yani <yani9900@gmail.com>
2025-01-09 21:06:14 +0200

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/using-hooks/",
   plugins: [react()],
+  base: "/using-hooks/",
 });
